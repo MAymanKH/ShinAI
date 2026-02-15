ce2cffa1e000fd7e00cf072401999192bd7b6c77
MAymanKH <mohamedayman011324@gmail.com>
2026-02-15 22:48:43 +0200

    Fix a bug

--- shin_ai/core/action_executor.py
+++ shin_ai/core/action_executor.py
@@ -224,7 +224,6 @@ async def _execute_mod_action(
                 ChatPermissions(
                     can_send_messages=True,
                     can_send_media_messages=True,
-                    can_send_other_messages=True,
                     can_add_web_page_previews=True,
                 )
             )
